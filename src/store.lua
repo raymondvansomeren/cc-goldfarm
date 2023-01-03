26c1d7e760b5a633066235a162866d07735b5d94
Raymond van Someren <raymondvansomeren@live.nl>
2023-01-04 00:10:54 +0100

    Push non-goldnuggets to trashcan

--- src/store.lua
+++ src/store.lua
@@ -1,11 +1,18 @@
 local peri = peripheral.getNames()
 
+local selfid = "turtle_xx"
 local trashcanid = "turtle_xx"
 local storage = {}
 
+local self = peripheral.wrap(selfid)
+if self == nil then
+    printError("No such turtle found. Fill in the name of this turtle in src/store.lua")
+    return
+end
+
 local trashcan = peripheral.wrap(trashcanid)
 if trashcan == nil then
-    printError("No such trashcan found. Fill in a connected trashcanid in store.lua")
+    printError("No such trashcan found. Fill in a connected trashcanid in src/store.lua")
     return
 end
 
@@ -18,3 +25,10 @@ local function pullStorage()
     end
 end
 
+for i=1, 16 do
+    -- turtle.select(i)
+    if turtle.getItemDetail(i).name == not "minecraft:gold_nugget" then
+        print(turtle.getItemDetail(i).name)
+        self.pushItems(trashcanid, i)
+    end
+end
